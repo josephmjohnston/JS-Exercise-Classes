a66e28b25aa6b809cefc4ca70d890a43127e6e93
josephmjohnston <josephmjohnston9985@gmail.com>
2020-05-14 20:08:00 -0500

    3 done.

--- index.js
+++ index.js
@@ -41,9 +41,24 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible) {
+    if (this.stomach.length < 10) {
+      return this.stomach.push(edible);
+    }
+    
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name},${this.age}`;
+  }
 }
-
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,8 +74,30 @@ class Person {
 */
 
 class Car {
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons) {
+      this.tank = this.tank + gallons;
+    }
+    drive(distance) {
+      let maxDistance = this.tank * this.milesPerGallon;
+    if (distance < maxDistance){
+      this.odometer += distance;
+      this.tank -= distance/this.milesPerGallon;
+    } else if (distance >= maxDistance){
+      this.odometer += maxDistance;
+      this.tank -= maxDistance / this.milesPerGallon;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+
+  }
 
 }
+
 
 /*
   TASK 3
@@ -75,7 +112,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+    constructor (attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;  
+  }
+    speak() {
+      return  `Hello my name is ${this.name}, I am from ${this.location}`
+    }
 }
 
 /*
